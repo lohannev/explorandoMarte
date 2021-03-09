5b88f70a89966b9d332945a7950404d4d8ddc43a
Lohanne Villela <70101845+lohannev@users.noreply.github.com>
2021-03-09 20:23:25 -0300

    Add files via upload

---
+++ marsRover/src/Malha.java
@@ -0,0 +1,21 @@
+
+package mars.rover;
+
+public class Malha {
+
+    private final int x;
+    private final int y;
+
+    public Malha(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ marsRover/src/PositionCode.java
@@ -0,0 +1,7 @@
+package mars.rover;
+
+enum PositionCode {
+    M,
+    L,
+    R,
+}
